df487266b2d4de07e3f12fb4d9632a0dd7040a47
aravind-kumar <karavindkumar1993@gmail.com>
2017-12-17 22:13:58 -0500

    Adding examples for the first exercise

---
+++ OCaml_from_the_beginning/Chapter_1/examples.ml
@@ -0,0 +1,16 @@
+1 + 2 * 3;; (* results in 7 as multiplication has more precendece*)
+
+
+5 * -2;;
+
+(* modulo operator is mod *)
+
+99>100;; (*results in a boolean*)
+
+
+(* <> is the not equal to opearator*)
+
+if 100<99
+then true
+else false;;
+
