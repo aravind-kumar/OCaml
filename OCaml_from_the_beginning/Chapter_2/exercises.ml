65c6b0ee72b574a3697359e24ca7f418b59182e3
aravind-kumar <karavindkumar1993@gmail.com>
2017-12-21 19:07:17 -0500

    Added new answers

--- OCaml_from_the_beginning/Chapter_2/exercises.ml
+++ OCaml_from_the_beginning/Chapter_2/exercises.ml
@@ -15,3 +15,18 @@ let rec sum2 arg1 =
   match arg1 with
     0 -> 0
   | _ -> arg1 + sum2 (arg1 - 1);;
+
+let rec pow base exp =
+  if exp = 0 then 1
+  else if exp = 1 then base
+  else if ((exp mod 2) = 0) then
+    pow (base*base) (exp/2)
+  else base * pow(base*base) (exp/2);;
+
+let rec pow2 base exp =
+  match exp with
+    0 -> 1
+  | 1 -> base
+  | exp when ((exp mod 2) = 0) ->
+    pow2 (base*base) (exp/2)
+  | _ -> base * pow2 (base*base) (exp/2);;
