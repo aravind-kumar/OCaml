a8087cd48daf57d53edd2b1eea1652f547c6e6ed
aravind-kumar <karavindkumar1993@gmail.com>
2017-12-23 23:53:14 -0500

    Adding comments

--- tuples.ml
+++ tuples.ml
@@ -1,12 +1,15 @@
 
+(*Tuples let you combine different items together*)
 let first_tuple = (3,"three");;
 
 let (x,y) = first_tuple in (x*x);;
 
-
+(*Tuple are obtained using pattern matching *)
 let distance (x1,y1) (x2,y2) =
   sqrt(
       ((x1 -. x2)**2.) +.
       ((y1 -. y2)**2.));;
+
+let values = distance (2.,3.) (3.,4.);;
 
 
