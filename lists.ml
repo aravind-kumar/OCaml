fe670346c7362b2f1b3438b5c923d0f2e0e1a005
aravind-kumar <karavindkumar1993@gmail.com>
2017-12-24 20:54:36 -0500

    adding changes

--- lists.ml
+++ lists.ml
@@ -1,9 +1,11 @@
+
 (*Lists are similar to tuples but can only hold data of the same type*)
 
 (*However lists would have to be seperated by semicolonns*)
 let list1 = [1;2;3;4];;
 
-let list2 = ["Ab" ;"BC";"C";"D"];;
+let list2 = ["Ab";"BC";"C";"D"];;
 
 List.length list2;;
+
 List.map list2 ~f:String.length;;
