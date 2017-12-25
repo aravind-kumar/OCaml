ac850940061c724f7086f98b93e73ed55c495292
aravind-kumar <karavindkumar1993@gmail.com>
2017-12-24 22:53:30 -0500

    Adding pattern matching for the list element

--- lists.ml
+++ lists.ml
@@ -17,3 +17,32 @@ let list3 = "ABC" :: list2;;
 (*@ operator is used to append two lists time proposnal to the lenght of the first list*)
 
 let list4 = list3 @ list2;;
+
+
+
+(*Pattern matching with Lists in OCAML*)
+
+
+let getFirst givenList =
+  match givenList with
+    first::rest -> first
+  |
+    _ -> "Invalid list" ;;
+
+getFirst list4;;
+
+
+(*Here we are using pattern matching to segrate the first and the rest of the list the first element is names as first and the rest are named as rest *)
+
+
+let getSecond givenList =
+  match givenList with
+    first::second::rest -> second
+  |
+    [] -> "Empty list"
+  |
+    [first] -> first;;
+
+getSecond list4;;
+
+(*Here we are using pattern matching to get the second element of the list which is named as second*)
