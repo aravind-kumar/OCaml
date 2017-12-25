414532e617d8db9e421f6370c824cd1d97880d5e
aravind-kumar <karavindkumar1993@gmail.com>
2017-12-24 22:46:38 -0500

    Adding list examples

--- lists.ml
+++ lists.ml
@@ -9,3 +9,11 @@ let list2 = ["Ab";"BC";"C";"D"];;
 List.length list2;;
 
 List.map list2 ~f:String.length;;
+
+(*:: operator is similar to cons operator used for appending*)
+
+let list3 = "ABC" :: list2;;
+
+(*@ operator is used to append two lists time proposnal to the lenght of the first list*)
+
+let list4 = list3 @ list2;;
